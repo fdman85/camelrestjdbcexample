380f450388554eb7c30f910e08b3e0c80e9cd333
fdman <fdman@mail.ru>
2015-09-30 19:33:05 +0400

    fixed error of updating methods, incompatible versions

--- src/main/java/com/peterservice/example/camel/restjdbc/ExampleUsersRouteConfiguration.java
+++ src/main/java/com/peterservice/example/camel/restjdbc/ExampleUsersRouteConfiguration.java
@@ -17,28 +17,37 @@
 
 package com.peterservice.example.camel.restjdbc;
 
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 public class ExampleUsersRouteConfiguration extends RouteBuilder {
 
+    private Processor headerMovingProcessor;
+
+    public ExampleUsersRouteConfiguration(HeaderMovingProcessor headerMovingProcessor) {
+        this.headerMovingProcessor = headerMovingProcessor;
+    }
+
     @Override
     public void configure() {
         from("restlet:/users?restletMethod=POST")
-                .setBody(simple("insert into users(firstName, lastName) values('${header.firstName}','${header.lastName}')"))
+                .process(headerMovingProcessor)
+                .setBody(simple("insert into users(firstName, lastName) values('${headers.firstName}','${headers.lastName}')"))
                 .to("jdbc:dataSource")
                 .setBody(simple("select * from users where id in (select max(id) from users)"))
                 .to("jdbc:dataSource");
 
         from("restlet:/users/{userId}?restletMethods=GET,PUT,DELETE")
+                .process(headerMovingProcessor)
                 .choice()
-                    .when(simple("${header.CamelHttpMethod} == 'GET'"))
-                        .setBody(simple("select * from users where id = ${header.userId}"))
-                    .when(simple("${header.CamelHttpMethod} == 'PUT'"))
-                        .setBody(simple("update users set firstName='${header.firstName}', lastName='${header.lastName}' where id = ${header.userId}"))
-                    .when(simple("${header.CamelHttpMethod} == 'DELETE'"))
-                        .setBody(simple("delete from users where id = ${header.userId}"))
-                    .otherwise()
-                        .stop()
+                .when(simple("${header.CamelHttpMethod} == 'GET'"))
+                .setBody(simple("select * from users where id = ${header.userId}"))
+                .when(simple("${header.CamelHttpMethod} == 'PUT'"))
+                .setBody(simple("update users set firstName='${header.firstName}', lastName='${header.lastName}' where id = ${header.userId}"))
+                .when(simple("${header.CamelHttpMethod} == 'DELETE'"))
+                .setBody(simple("delete from users where id = ${header.userId}"))
+                .otherwise()
+                .stop()
                 .end()
                 .to("jdbc:dataSource");
 
@@ -46,5 +55,7 @@ public class ExampleUsersRouteConfiguration extends RouteBuilder {
                 .setBody(simple("select * from users"))
                 .to("jdbc:dataSource");
     }
+
+
 }
 
